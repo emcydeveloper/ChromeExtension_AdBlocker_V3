c24668c5c8f74a79565ba5f3cb60276a4e04d33f
Ganesh Kanna MC <emcydeveloper@gmail.com>
2022-07-04 17:05:32 +0530

    Codeing optimized

--- background.js
+++ background.js
@@ -5,139 +5,126 @@ chrome.tabs.onUpdated.addListener((getTabDetailsOnUpdate, changeInfo, tab) => {
   // chrome.declarativeNetRequest.GETMATCHEDRULES_QUOTA_INTERVAL = 1;
   // chrome.declarativeNetRequest.MAX_GETMATCHEDRULES_CALLS_PER_INTERVAL = 100;
 
-  console.log(changeInfo.status);
-  // chrome.declarativeNetRequest.updateDynamicRules()
-  if (changeInfo.status === "loading") {
-    chrome.declarativeNetRequest.getDynamicRules((a) =>
-      console.log("Before Deleting ***********************************", a)
-    );
-    let deleteItems = [];
-    let formRules = [];
-    let blockUrls = [];
-    // let _blockUrls = ["https://www.google.com/"];
-
-    chrome.declarativeNetRequest.getDynamicRules((getItm) => {
-      chrome.storage.sync.get("dynBlockSite", ({dynBlockSite}) => {
-
-        if(dynBlockSite.length>0){
-          dynBlockSite.map((item) => blockUrls = [...blockUrls, item.url]);
-          console.log("URLS to block : ", blockUrls);
-          getItm.map((itm) => {
-            deleteItems = [...deleteItems, itm.id];
-          });
-          console.log("deleteItems~~~~~~~~~~~~~~~~", deleteItems);
-    
-          console.log("Deleting/Adding Rules");
-    
-          blockUrls.forEach((domain, index) => {
-            let id = index + 1;
-            formRules = [
-              ...formRules,
-              {
-                id: id,
-                priority: 1,
-                action: { type: "block" },
-                condition: { urlFilter: domain, resourceTypes: ["main_frame"] },
-              },
-            ];
-          });
-          console.log("formRules",formRules);
-          chrome.declarativeNetRequest.updateDynamicRules({removeRuleIds: deleteItems, addRules: formRules });
-          chrome.declarativeNetRequest.getDynamicRules((a) =>
-            console.log(
-              "After block rule updated ***********************************",
-              a
-            )
-          );
-        }else{
-          getItm.map((itm) => {
-            deleteItems = [...deleteItems, itm.id];
-          });
-          console.log("deleteItems~~~~~~~~~~~~~~~~", deleteItems);
-    
-          console.log("Deleting/Adding Rules");
-    
-          blockUrls.forEach((domain, index) => {
-            let id = index + 1;
-            formRules = [
-              ...formRules,
-              {
-                id: id,
-                priority: 1,
-                action: { type: "block" },
-                condition: { urlFilter: domain, resourceTypes: ["main_frame"] },
-              },
-            ];
-          });
-          console.log("formRules",formRules);
-          chrome.declarativeNetRequest.updateDynamicRules({removeRuleIds: deleteItems, addRules: formRules });
-          chrome.declarativeNetRequest.getDynamicRules((a) =>
-            console.log(
-              "After block rule updated ***********************************",
-              a
-            )
-          );
-        }
-      });
- 
-    });
-  }
-
-  if (changeInfo.status === "complete") {
-    console.log(changeInfo);
-    chrome.declarativeNetRequest.getMatchedRules(
-      { tabId: getTabDetailsOnUpdate },
-      (matchedRulesInTab) => {
-        if (matchedRulesInTab != undefined) {
-          let matchedRuleCount = matchedRulesInTab.rulesMatchedInfo.length;
-
-          console.log(
-            "Tab ID: ",
-            getTabDetailsOnUpdate,
-            "|| Rules Matched: ",
-            matchedRulesInTab.rulesMatchedInfo
-          );
-
-          setBadgeCount(matchedRuleCount, getTabDetailsOnUpdate);
-
-          console.log("Before Found - ", getMatchedRuleCounts);
-          let found = getMatchedRuleCounts.some(
-            (item) => item.tabId === getTabDetailsOnUpdate
-          );
-
-          if (!found) {
-            getMatchedRuleCounts = [
-              ...getMatchedRuleCounts,
-              {
-                tabId: tab.id,
-                URL: tab.url,
-                matchedCount: matchedRuleCount,
-                matchedRules: matchedRulesInTab.rulesMatchedInfo,
-              },
-            ];
-          }
-
-          if (found) {
-            getMatchedRuleCounts.map((item, index) => {
-              if (item.tabId == getTabDetailsOnUpdate) {
-                getMatchedRuleCounts[index].URL = tab.url;
-                getMatchedRuleCounts[index].matchedCount = matchedRuleCount;
-                getMatchedRuleCounts[index].matchedRules =
-                  matchedRulesInTab.rulesMatchedInfo;
-              }
-            });
-          }
-          console.log("Tab Updated", getMatchedRuleCounts);
-          chrome.storage.sync.set({ getMatchedRuleCounts });
-        } else {
-          console.log("Rule test limit exceed - ", matchedRulesInTab);
-          console.log("Captured Data - ", getMatchedRuleCounts);
-        }
-      }
-    );
+  switch (changeInfo.status) {
+    case "loading":
+      chrome.declarativeNetRequest.getDynamicRules(dynamicRulesHandler);
+      break;
+    case "complete":
+      getRulesStatus(getTabDetailsOnUpdate, tab);
+      break;
+    default:
+      break;
   }
 });
 
 function setBadgeCount(count = 0, tabId) {
   chrome.action.setBadgeText({ text: count.toString(), tabId: tabId });
+}
+
+
+
+function getRulesStatus(getTabDetailsOnUpdate, tab) {
+
+  chrome.declarativeNetRequest.getMatchedRules(
+    { tabId: getTabDetailsOnUpdate },
+    (matchedRulesInTab) => {
+      if (matchedRulesInTab != undefined) {
+        let matchedRuleCount = matchedRulesInTab.rulesMatchedInfo.length;
+
+
+        setBadgeCount(matchedRuleCount, getTabDetailsOnUpdate);
+
+
+        let found = getMatchedRuleCounts.some(
+          (item) => item.tabId === getTabDetailsOnUpdate
+        );
+
+        if (!found) {
+          getMatchedRuleCounts = [
+            ...getMatchedRuleCounts,
+            {
+              tabId: tab.id,
+              URL: tab.url,
+              matchedCount: matchedRuleCount,
+              matchedRules: matchedRulesInTab.rulesMatchedInfo,
+            },
+          ];
+        }
+
+        if (found) {
+          getMatchedRuleCounts.map((item, index) => {
+            if (item.tabId == getTabDetailsOnUpdate) {
+              getMatchedRuleCounts[index].URL = tab.url;
+              getMatchedRuleCounts[index].matchedCount = matchedRuleCount;
+              getMatchedRuleCounts[index].matchedRules =
+                matchedRulesInTab.rulesMatchedInfo;
+            }
+          });
+        }
+ 
+        chrome.storage.sync.set({ getMatchedRuleCounts });
+      } else {
+        console.error("Rule test limit exceed - ", matchedRulesInTab);
+        console.error("Captured Data - ", getMatchedRuleCounts);
+      }
+    }
+  );
+}
+
+function updateDynamicRules(deleteItems, formRules) {
+  chrome.declarativeNetRequest.updateDynamicRules({
+    removeRuleIds: deleteItems,
+    addRules: formRules,
+  });
+}
+
+
+function dynamicRulesHandler(getItm)  {
+  let deleteItems = [];
+  let formRules = [];
+  let blockUrls = [];
+  chrome.storage.sync.get("dynBlockSite", ({ dynBlockSite }) => {
+    if (dynBlockSite.length > 0) {
+      dynBlockSite.map((item) => (blockUrls = [...blockUrls, item.url]));
+      console.log("URLS to block : ", blockUrls);
+      getItm.map((itm) => {
+        deleteItems = [...deleteItems, itm.id];
+      });
+
+      blockUrls.forEach((domain, index) => {
+        let id = index + 1;
+        formRules = [
+          ...formRules,
+          {
+            id: id,
+            priority: 1,
+            action: { type: "block" },
+            condition: { urlFilter: domain, resourceTypes: ["main_frame"] },
+          },
+        ];
+      });
+
+      updateDynamicRules(deleteItems, formRules);
+      // chrome.declarativeNetRequest.getDynamicRules((a) =>console.log(a));
+    } else {
+      getItm.map((itm) => {
+        deleteItems = [...deleteItems, itm.id];
+      });
+
+      blockUrls.forEach((domain, index) => {
+        let id = index + 1;
+        formRules = [
+          ...formRules,
+          {
+            id: id,
+            priority: 1,
+            action: { type: "block" },
+            condition: { urlFilter: domain, resourceTypes: ["main_frame"] },
+          },
+        ];
+      });
+
+      updateDynamicRules(deleteItems, formRules);
+    }
+  });
 }
